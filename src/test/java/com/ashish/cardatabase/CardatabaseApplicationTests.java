7faa40ab2c18022cf60cfb28e99d8411afe3c626
Ashish Sharda <ashishjsharda@gmail.com>
2022-09-04 06:10:17 -0500

    first commit

---
+++ src/test/java/com/ashish/cardatabase/CardatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashish.cardatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardatabaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
