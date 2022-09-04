360f49a85786211c9df266f974c5161a991989f6
Ashish Sharda <ashishjsharda@gmail.com>
2022-09-04 06:12:26 -0500

    Add Logger

--- src/main/java/com/ashish/cardatabase/CardatabaseApplication.java
+++ src/main/java/com/ashish/cardatabase/CardatabaseApplication.java
@@ -1,11 +1,14 @@
 package com.ashish.cardatabase;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CardatabaseApplication {
+public class    CardatabaseApplication {
 
+    private static final Logger logger = LoggerFactory.getLogger(CardatabaseApplication.class);
     public static void main(String[] args) {
         SpringApplication.run(CardatabaseApplication.class, args);
     }
