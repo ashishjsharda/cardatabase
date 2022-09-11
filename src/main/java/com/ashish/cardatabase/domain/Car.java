6f4a1b9fed1b230a10887481268c046c1a4a958d
Ashish Sharda <ashishjsharda@gmail.com>
2022-09-11 08:00:49 -0500

    Add Car Class

---
+++ src/main/java/com/ashish/cardatabase/domain/Car.java
@@ -0,0 +1,93 @@
+package com.ashish.cardatabase.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Car {
+    private String brand, model, color, registerNumber;
+    private int year, price;
+    private Long id;
+
+    public Car() {
+    }
+
+    public Car(String brand, String model, String color, String registerNumber, int year, int price) {
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+        this.registerNumber = registerNumber;
+        this.year = year;
+        this.price = price;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getRegisterNumber() {
+        return registerNumber;
+    }
+
+    public void setRegisterNumber(String registerNumber) {
+        this.registerNumber = registerNumber;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", color='" + color + '\'' +
+                ", registerNumber='" + registerNumber + '\'' +
+                ", year=" + year +
+                ", price=" + price +
+                '}';
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Id
+    public Long getId() {
+        return id;
+    }
+}
+
